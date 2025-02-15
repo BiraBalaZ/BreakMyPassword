2d412306d1eceb4660061202fff6f5119801446b
BiraBalaZ <erick.m.anjos@gmail.com>
2025-02-15 00:43:34 -0300

    Creating and Uploading Break My Password

---
+++ README.md
@@ -0,0 +1,8 @@
+# Break My Password
+
+A while ago in a circle of friends, one of us suggested a game. Which consisted of the challenger thinking of a 4-digit password. And the rest of us have to guess.
+
+But with one detail. It will only tell you how many numbers are correct if they are in the right place. Each person speaks once, for example:
+The challenger thought of the password 1234. Player A comes up with 6270, so the challenger says: A correct number, as number 2 is in the same position. And the suggestions continue for Player B, C... Until the wheel starts again and the challenger's password is correct. Ideally, everyone should be paying close attention to the numbers and even writing them down.
+
+Well, this is a Python version of the game that entertained me and my friends for a whole night.
---
+++ main.py
@@ -0,0 +1,75 @@
+from random import randrange
+from os import system
+system('cls')
+
+def leftGame():
+    system('cls')
+    print('You decided to leave.')
+    exit(1)
+
+def checkHits():
+    return sum(1 for i in range(4) if password[i] == userInput[i])
+
+password = [str(randrange(10)), str(randrange(10)), str(randrange(10)), str(randrange(10))]
+
+# Set difficult
+print('What difficulty do you want to play?\n\n\033[32m[1] - Easy\033[m\n\033[33m[2] - Medium\033[m\n\033[31m[3] - Hard\033[m\n\033[34m[Other] - Exit\033[m')
+difficult = str(input('>>>'))
+
+if difficult == '1':
+    digits = 4
+elif difficult == '2':
+    digits = 6
+elif difficult == '3':
+    digits = 8
+else:
+    leftGame()
+
+system('cls')
+print(f'I thought of a {digits} digit password. Can you guess?')
+
+if difficult == '3':
+    print('\n\033[31mThe terminal will not save what you sent.\nYou´re fucked up.\033[35m >:)\033[m\n')
+
+print('\033[33mSend any letter to leave.\033[m')
+
+# Showing password for debug
+# print(password)
+
+userInput = None
+
+while True:
+    suggest = str(input('>>>'))
+
+    if not suggest.isdigit():  
+        leftGame()
+
+    while len(suggest) < digits:
+        system('cls')
+
+        if difficult != '3':
+            if userInput != None:
+                print(f'Última opção válida inserida{userInput}')
+                print(f'Teve {checkHits()} acertos')
+
+        print(f'\033[31mYou must enter at least {digits} digits\033[m')
+
+        # Getting user suggestion
+        suggest = str(input('>>>'))
+
+        if not suggest.isdigit():  
+            leftGame()
+    
+    userInput = [suggest[i] for i in range(4)]
+
+    # Checking the numbers
+    checkHits()
+
+    if difficult == '3':
+        system('cls')
+
+    print(f'{checkHits()} correct numbers')
+
+    if checkHits() == 4:
+        print("\n🎉 Congratulations! All numbers are correct! 🎉")
+        break
